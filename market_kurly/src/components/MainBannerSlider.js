949dda293ba49c6676a9520f73c8ecde00470580
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-17 15:19:23 +0900

    Fix MainBannerSlider
    슬라이더 뼈대 갖고 왔습니다
    슬릭 관련 css 패키지도 갖고 왔습니다
    스타일컴포넌트도 받아왔습니다
    계속 수정 예정입니다

--- market_kurly/src/components/MainBannerSlider.js
+++ market_kurly/src/components/MainBannerSlider.js
@@ -1,13 +1,44 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const MainBannerSlider = (props) => {
+  let settings = {
+    // dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <React.Fragment>
       <div>
-
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>사진1</h3>
+          </div>
+          <div>
+            <h3>사진2</h3>
+          </div>
+          <div>
+            <h3>사진3</h3>
+          </div>
+          <div>
+            <h3>사진4</h3>
+          </div>
+          <div>
+            <h3>사진5</h3>
+          </div>
+          <div>
+            <h3>사진6</h3>
+          </div>
+        </Slider>
       </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default MainBannerSlider;
--- market_kurly/src/pages/Main.js
+++ market_kurly/src/pages/Main.js
@@ -1,11 +1,20 @@
 import React from "react";
+import MainBannerSlider from "../components/MainBannerSlider";
+import styled from "styled-components";
 
 const Main = (props) => {
   return (
     <React.Fragment>
-      <div></div>
+      <MainBannerWrap>
+        <MainBannerSlider/>
+      </MainBannerWrap>
     </React.Fragment>
   )
 }
+
+const MainBannerWrap = styled.div`
+  margin: 0px auto;
+  max-width: 100%;
+`;
 
 export default Main;
