adccc0cb845cdb475a62fc348811ecde9f5c11a3
Ben <72005634+prodoben@users.noreply.github.com>
2021-07-19 09:36:33 +0900

    Header connected to Main
    Main 안에 Header 연결

--- market_kurly/src/pages/Main.js
+++ market_kurly/src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MainBannerSlider from "../components/MainBannerSlider";
 import styled from "styled-components";
+import Header from "../components/Header"
 
 const Main = (props) => {
   return (
     <React.Fragment>
+      <Header></Header>
         <MainBannerSlider/>
     </React.Fragment>
   )
