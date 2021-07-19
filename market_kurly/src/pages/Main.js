83baf1b01c0e6fd1480d935b5be52021d8d351a3
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-19 20:37:07 +0900

    Fix Main
    다시 삭제할거같은데 일단 옮겨놨어요!

--- market_kurly/src/pages/Main.js
+++ market_kurly/src/pages/Main.js
@@ -5,20 +5,18 @@ import styled from "styled-components";
 import Header from "../components/Header"
 import MainMiddleBanner from "../components/MainMiddleBanner";
 import MainSpecialBanner from "../components/MainSpecialBanner";
-
+const title = [
+  {
+    id: 0,
+    title: "이 상품 어때요?"
+  },
+  {
+    id: 1,
+    title: "놓치면 후회할 가격"
+  }
+]
 
 const Main = (props) => {
-  const title = [
-    {
-      id: 0,
-      title: "이 상품 어때요?"
-    },
-    {
-      id: 1,
-      title: "놓치면 후회할 가격"
-    }
-  ]
-  console.log(title[0])
   return (
     <React.Fragment>
       <Header></Header>
