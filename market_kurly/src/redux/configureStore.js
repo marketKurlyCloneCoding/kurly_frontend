0caba37dc49977b36a0d8fba019bbef1eecba369
IL KYO OH <81012135+qdaszx@users.noreply.github.com>
2021-07-20 15:07:56 +0900

    Add redux
    메인에서 3개에 배너를 백에서 받아오는 리덕스를 만들었습니다

---
+++ market_kurly/src/redux/configureStore.js
@@ -0,0 +1,34 @@
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
+import { connectRouter } from "connected-react-router";
+
+import Product from "../redux/modules/product";
+
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  product: Product,
+  router: connectRouter(history),
+});
+
+const middlewares = [thunk.withExtraArgument({ history: history })];
+
+const env = process.env.NODE_ENV;
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+
+let store = (initialStore) => createStore(rootReducer, enhancer);
+
+export default store();
---
+++ market_kurly/src/redux/modules/product.js
@@ -0,0 +1,137 @@
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+import axios from "axios";
+
+const SET_OFFER_PRODUCT = "SET_OFFER_PRODUCT";
+const SET_SPECIAL_PRODUCT = "SET_SPECIAL_PRODUCT";
+const SET_HOT_PRODUCT = "SET_HOT_PRODUCT";
+
+const setOfferDealProduct = createAction(SET_OFFER_PRODUCT, (product_list) => ({
+  product_list,
+}));
+const setSpecialDealProduct = createAction(
+  SET_SPECIAL_PRODUCT,
+  (product_list) => ({
+    product_list,
+  })
+);
+const setHotDealProduct = createAction(SET_HOT_PRODUCT, (product_list) => ({
+  product_list,
+}));
+
+const initialState = {
+  Offerlist: [],
+  Speciallist: [],
+  Hotlist: [],
+};
+
+const getOfferDealProductAPI = () => {
+  const product_API = "http://localhost/api/v1/offer_deal";
+  return function (dispatch, getState, { history }) {
+    axios
+      .get(product_API)
+      .then((res) => {
+        console.log(res);
+        let product_list = [];
+        let response_data = res.data;
+        response_data.forEach((rd) => {
+          let product = {
+            id: rd.id,
+            title: rd.title,
+            price: rd.price,
+            original_price: rd.original_price,
+            img: rd.img,
+            dc: rd.dc,
+          };
+          product_list.push(product);
+        });
+        dispatch(setOfferDealProduct(product_list));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
+const getSpecialDealProductAPI = () => {
+  const product_API = "http://localhost/api/v1/special_deal";
+  return function (dispatch, getState, { history }) {
+    axios
+      .get(product_API)
+      .then((res) => {
+        console.log(res);
+        let product_list = [];
+        let response_data = res.data;
+        response_data.forEach((rd) => {
+          let product = {
+            id: rd.id,
+            title: rd.title,
+            subTitle: rd.subTitle,
+            price: rd.price,
+            original_price: rd.original_price,
+            img: rd.img,
+            dc: rd.dc,
+          };
+          product_list.push(product);
+        });
+        dispatch(setSpecialDealProduct(product_list));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
+const getHotDealProductAPI = () => {
+  const product_API = "http://localhost/api/v1/hot_deal";
+  return function (dispatch, getState, { history }) {
+    axios
+      .get(product_API)
+      .then((res) => {
+        console.log(res);
+        let product_list = [];
+        let response_data = res.data;
+        response_data.forEach((rd) => {
+          let product = {
+            id: rd.id,
+            title: rd.title,
+            price: rd.price,
+            original_price: rd.original_price,
+            img: rd.img,
+            dc: rd.dc,
+          };
+          product_list.push(product);
+        });
+        dispatch(setHotDealProduct(product_list));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
+
+export default handleActions(
+  {
+    [SET_OFFER_PRODUCT]: (state, action) =>
+      produce(state, (draft) => {
+        draft.Offerlist = action.payload.product_list;
+      }),
+    [SET_SPECIAL_PRODUCT]: (state, action) =>
+      produce(state, (draft) => {
+        draft.Speciallist = action.payload.product_list;
+      }),
+    [SET_HOT_PRODUCT]: (state, action) =>
+      produce(state, (draft) => {
+        draft.Hotlist = action.payload.product_list;
+      }),
+  },
+  initialState
+);
+
+const actionCreators = {
+  getOfferDealProductAPI,
+  getSpecialDealProductAPI,
+  getHotDealProductAPI,
+};
+
+export { actionCreators };
